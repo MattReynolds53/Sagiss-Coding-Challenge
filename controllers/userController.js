7bdc8286acada06f37fb48e1827215ee9159b549
Matt Reynolds <mattreynolds53@gmail.com>
2022-04-13 21:07:36 -0400

    I began building the userRoutes and userController files. I have created functions for retrieving all user information, individual user information, creating a user, updating a user, and deleting a user (still finishing deleteUser function).

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const { User } = require("../models");
+
+const userController = {
+  getAllUsers(req, res) {
+    User.find()
+      .select("-__v")
+      .then((users) => res.json(users))
+      .catch((err) => {
+        {
+          res.status(500).json(err);
+          console.log(err);
+        }
+      });
+  },
+
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "Uh oh, there seems to be no User with that ID.",
+            })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateUser(req, res) {
+      User.findOneAndUpdate(
+          { _id: req.params.id },
+          { $set: req.body },
+          { runValidators: true, new: true }
+      ).then((dbUserData) => {
+          if (dbUserData) {
+              res.status(404).json({ message: "Sorry, here is no User with that ID." });
+              return;
+          }
+          res.json(dbUserData);
+      }).catch((err) => res.json(err));
+  },
+};
